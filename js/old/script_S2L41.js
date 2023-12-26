1cca2873f0d6383968271fbdc010c87e1491db53
Vladimir <golik.vladimir@gmail.com>
2023-12-26 22:29:14 +0200

    add S2L41

---
+++ js/old/script_S2L41.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// //1
+// let x = 5;
+// alert(++x);
+
+// //2
+// console.log([ ] + false - null + true);
+
+// //3
+// let y = 1; let x = y = 2; console.log(x);
+
+// //4
+// console.log([ ] + 1 + 2);
+
+// //5
+// console.log("1"[0]);
+
+// //6
+// console.log(2 && 1 && null && 0 && undefined);
+
+// //7
+// console.log(!!( 2 && 1 ) === (2 && 1));
+
+// //8
+// console.log(null || 2 && 3 || 4 );
+
+// //9 
+// const a = [1, 2, 3];
+// const b = [1, 2, 3];
+// console.log(a == b);
+
+// //10
+// console.log(+"Infinity" );
+
+// //11
+// console.log("Ёжик" > "яблоко");
+
+//12
+console.log(0 || "" || 2 || undefined || true || falsе);
