7edba69142eefaffa721b32772922dc6f7aea541
Vladimir <golik.vladimir@gmail.com>
2023-12-28 22:07:22 +0200

    add S2L42

---
+++ js/old/script_S2L42.js
@@ -0,0 +1,44 @@
+"use strict";
+
+//by id
+const box = document.getElementById("box");
+console.log(box);
+
+//by Tag
+const btns = document.getElementsByTagName("button");
+const btn2 = document.getElementsByTagName("button")[1];
+console.log(btns);
+console.log(btns[0]);
+console.log(btn2);
+
+//by Name
+const circles = document.getElementsByClassName("circle");
+console.log(circles);
+console.log(circles[0]);
+
+//by scc selector
+const hearts = document.querySelectorAll(".heart");
+console.log(hearts);
+hearts.forEach(item => {
+	console.log(item);
+});
+
+const oneHeart = document.querySelector(".heart");
+console.log(oneHeart);
+
+//by xpath
+const xpathExpression = "//div[@class='circle']";
+const contextNode = document; // Выполняем запрос относительно всего документа
+
+const result = document.evaluate(xpathExpression, contextNode, null, XPathResult.ANY_TYPE, null);
+console.log(result);
+
+let node = result.iterateNext();
+while (node) {
+	console.log(node);
+	node = result.iterateNext();
+}
+
+
+const btn1 = document.body.valueOf(1);
+console.log(btn1);
